fcdbbcfc8faa8c32fdab1be8d61ea3bf0744e36f
Varsha Belligundu <680565@fre-u680565l001.ad.seagate.com>
2021-04-28 17:13:27 -0700

    updates test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,17 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-project'`, () => {
+  it(`should have as title 'My Stocks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-project');
+    expect(app.title).toEqual('My Stocks');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('My Stocks');
-  });
-
 });
